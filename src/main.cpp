a3ac67f0951d2ea870139670b0019125b8ee6448
Jannik Prinz <jannik.prinz@gmail.com>
2023-09-16 11:54:55 +0200

    Include BSEC library WIP

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include "bsec_datatypes.h"
+#include "bsec_interface.h"
 
 int main() {
     std::cout << "Hello, CMake project with C++20!" << std::endl;
     return 0;
+
+    bsec_version_t s;
+    auto x = bsec_init();
+    if (x == bsec_library_return_t::BSEC_OK)
+    {
+        std::cout << "Init success!" << std::endl;
+    }
+    else
+    {
+        std::cout << "Init failed: " << x << std::endl;
+    }
 }
